cf9dc0ce4fb6af1ceea44341bc2fc7478043db26
entpnrusman <entpnr.usman@gmail.com>
2023-07-19 17:32:58 +0500

    update

--- components/loader.tsx
+++ components/loader.tsx
@@ -11,7 +11,7 @@ export const Loader = () => {
         />
       </div>
       <p className="text-sm text-muted-foreground">
-        Genius is thinking...
+        UW Ai Bot is thinking...
       </p>
     </div>
   );
